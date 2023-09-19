0ee76446b2ca87f2affadc886281ce330021dac1
kalinggapadelmuhamad <enginerbros@gmail.com>
2023-09-19 20:51:23 +0700

    Update : Upate Auth Pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.blank-page', ["type_menu" => "dashbaord"]);
-});
+// Route::get('/', function () {
+//     return view('pages.blank-page', ["type_menu" => "dashbaord"]);
+// });
+
+Route::get('/', function(){
+    return view('pages.dashboard.dashboard-general-dashboard', ['type_menu' => '']);
+})->name('login');
+Route::get('/login', function(){
+    return view('auth.auth-login2');
+})->name('login');
+Route::get('/register', function(){
+    return view('auth.auth-register');
+})->name('resgiter');
+Route::get('/forgot', function(){
+    return view('auth.auth-forgot-password');
+})->name('forgot');
+Route::get('/reset/password', function(){
+    return view('auth.auth-reset-password');
+})->name('reset');
